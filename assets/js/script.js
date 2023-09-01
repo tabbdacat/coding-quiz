d56e91017ba9fd6b93edabfafa43ef94d032dc7c
Meow <tabbathapischke@gmail.com>
2023-08-31 14:49:47 -1000

    add questions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,21 @@ let timeRemaining = document.querySelector(".timer");
 const startBtn = document.querySelector("#start");
 const startScreenEl = document.querySelector("#start-screen");
 const questionsEl = document.querySelector("#test");
-const endScreenEl = document.querySelector("#end-screen")
+const endScreenEl = document.querySelector("#end-screen");
 // Selects element by id
 let time = document.getElementById("time");
+
+let scoreListText = document.createElement("li");
+const highScoreForm = document.querySelector("#high-score-form");
+const initialInputEl = document.querySelector("#initial-input");
+const submitScoreBtn = document.querySelector("#submit-score-btn");
+
+
+// submitScoreBtn.addEventListener ("click", function (event) {
+//   event.preventDefault();
+//   const highScoreList = document.querySelector(".high-score-list").textContent(initialInputEl.value)
+// highScoreForm.appendChild(highScoreList)
+// })
 
 
 startBtn.addEventListener ("click", function()  {
@@ -35,8 +47,26 @@ startScreenEl.classList.add("hide");
 questionsEl.classList.remove("hide");
 })
 
-endScreenElEl.classList.remove("hide");
-questionsEl.classList.add("hide");
+
+// where does this go??
+// endScreenElEl.classList.remove("hide");
+// questionsEl.classList.add("hide");
 
 
+// let questions = [  
+//     {
+//     question: "why is the sky blue?",
+//     answer1: ["cuz sad","cuz space","cuz i said so"]
+//     answer2: ["cuz i'm right"]
+//     }
+//     {
+//     question: "why not?",
+//     answer1: ["dunno", "shrug","ahhh"]
+//     answer2: ["cuz i'm right"]
+//         }
+// ]
+// var a = document.querySelector("#a")
+// var b = document.querySelector("#b")
+// var c = document.querySelector("#c")
+// var d = document.querySelector("#d")
 
