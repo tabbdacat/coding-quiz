839cf959fabff6bc9d91c536f0edac2e0893f59d
Meow <tabbathapischke@gmail.com>
2023-11-14 12:52:59 -1000

    fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
-let timeRemaining = document.querySelector(".timer");
+// Select html elements
+const timeRemaining = document.querySelector(".timer");
 const startBtn = document.querySelector("#start");
 const startScreenEl = document.querySelector("#start-screen");
 const highScoreDisplayEl = document.querySelector("#high-score-display");
 const questionsEl = document.querySelector("#test");
 const endScreenEl = document.querySelector("#end-screen");
-// Selects element by id
-let time = document.getElementById("#time");
-let questions = document.querySelector('#questions');
-let optionBtns = document.querySelectorAll('.optionBtn');
+const time = document.getElementById("#time");
+const questions = document.querySelector('#questions');
+const optionBtns = document.querySelectorAll('.optionBtn');
+const highScoreForm = document.querySelector("#high-score-form");
+const initialInputEl = document.querySelector("#initial-input");
+const submitScoreBtn = document.querySelector("#submit-score-btn");
+const highScoreList = document.querySelector("#high-score-list");
+const curScore = document.querySelector("#cur-score");
+const clearHistoryBtn = document.querySelector("#clearHistoryBtn");
+// retrieve items from local storage or set as empty array
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 let currentQuestionIndex = 0;
 let secondsLeft;
 
-
+// start timer
 startBtn.addEventListener("click", function () {
   // time for quiz
   secondsLeft = 60;
@@ -19,18 +28,25 @@ startBtn.addEventListener("click", function () {
   let timerInterval = setInterval(function () {
     // subtracting 1 from seconds left
     secondsLeft--;
-
     timeRemaining.textContent = secondsLeft + " seconds left.";
 
+    function stopTimer() {
+      clearInterval(timerInterval);
+      timeRemaining.textContent = ""
+    };
+
+    // end quiz if timer runs out
     if (secondsLeft <= 0) {
+      console.log("im hit!");
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Clears timer text 
-      timeRemaining.textContent = ""
-
+      timeRemaining.textContent = "";
+      endScreenEl.classList.remove("hide");
+      questionsEl.classList.add("hide");
+      highScoreDisplayEl.classList.remove("hide");
     }
   }, 1000);
-
 
   startScreenEl.classList.add("hide");
   highScoreDisplayEl.classList.add("hide");
@@ -38,7 +54,7 @@ startBtn.addEventListener("click", function () {
   displayQuestion(currentQuestionIndex);
 })
 
-// question list object
+// question list array
 let myQuestions = [
   {
     quest: "Which of the following function of Array object adds one or more elements to the end of an array and returns the new length of the array?",
@@ -57,6 +73,7 @@ let myQuestions = [
   }
 ]
 
+// display questions with options
 function displayQuestion(i) {
   let currentQuestionText = myQuestions[i].quest;
 
@@ -67,104 +84,107 @@ function displayQuestion(i) {
   optionBtns[3].textContent = myQuestions[i].options[3];
 }
 
-// const rightAnswer= document.querySelector("#right-answer");
-
-// function correct() {
-//   rightAnswer.classList.remove("hide");
-// }
-// function removeCorrect() {
-//   rightAnswer.classList.remove("hide");
-// }
-
 optionBtns.forEach(function (x) {
-
+  // listens for correct answer
   x.addEventListener("click", function (e) {
     if (e.target.textContent === myQuestions[currentQuestionIndex].answer) {
       alert("That's correct!");
-      // setTimeout(correct(), 1000);
-      // setTimeout(removeCorrect(), 1000);
     } else {
       alert("Wrong Answer! 10 seconds deducted from timer.");
       secondsLeft = secondsLeft - 10;
     }
 
+    // increase question index by 1 after click
     currentQuestionIndex++;
 
-
+    // end quiz if at end of question list
     if (currentQuestionIndex >= myQuestions.length) {
-      endOfQuiz()
-
-
+      endOfQuiz();
       return
     }
+
+    // display next question
     displayQuestion(currentQuestionIndex);
   })
 });
 
-
+// change user view
 function endOfQuiz() {
-  endScreenEl.classList.remove("hide");
-  questionsEl.classList.add("hide");
-  highScoreDisplayEl.classList.remove("hide");
+  // set score to seconds remaining
   curScore.textContent = secondsLeft;
-  secondsLeft = 0;
+  if(secondsLeft > 0) {
+    secondsLeft = 0;
+  };
 }
 
-const highScoreForm = document.querySelector("#high-score-form");
-const initialInputEl = document.querySelector("#initial-input");
-const submitScoreBtn = document.querySelector("#submit-score-btn");
-const highScoreList = document.querySelector("#high-score-list");
-const curScore = document.querySelector("#cur-score");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const clearHistoryBtn = document.querySelector("#clearHistoryBtn");
-
+function displayHighScores() {
+  highScoreList.innerHTML = "";
+  for (var i = 0; i < highScores.length; i++) {
+console.log(highScores);
+console.log(highScores[i].score)
+var li = document.createElement("li");
+li.textContent = "Score: " + highScores[i].score + "  Name: " + highScores[i].initials;
+// places list item in high score list
+highScoreList.appendChild(li);
+  }
+}
 
 submitScoreBtn.addEventListener("click", function (event) {
   // prevent page refresh
   event.preventDefault();
-// calls high score function
-  submitHighScores();
+  // calls high score function
+  // submitHighScores();
   updateHighScores(initialInputEl.value, JSON.parse(curScore.textContent));
+  init();
 })
 
- // input intials and score to local storage
+// input intials and score to local storage
 function updateHighScores(initialInput, curScore) {
   const result = { initials: initialInput, score: curScore };
 
   highScores.push(result);
-  // Sort high scores high to low
-  highScores.sort((a, b) => b.score - a.score)
-  // keep 5 highest scores
-  highScores.splice(5);
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-}
 
-function submitHighScores() {
-  let initialArr = [];
-  let scoreArr = [];
-  highScoreList.innerHTML = "";
-  // creates variable for value input of initials entered, minus white spaces
   var initialText = initialInputEl.value.trim();
-
-  // for (var i = 0; i < initialArr.length; i++) {
-  //   // create variable for current initial in iteration
-  //   var initial = InitialArr[i];
-
-  // adds input to initial array
-  initialArr.push(initialText);
-  // creates list item
   var li = document.createElement("li");
   // setting text of the li to the current initial entered
-  li.textContent = "Score: " + curScore.textContent + "  Name: " + initialText;
-  // places list item in high score list
-  highScoreList.appendChild(li);
+  li.textContent = "Score: " + curScore + "  Name: " + initialText;
+
+    // Sort high scores high to low
+    highScores.sort((a, b) => b.score - a.score)
+    // keep 5 highest scores
+    highScores.splice(5);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
 }
 
+// function submitHighScores() {
+//   let initialArr = [];
+//   let scoreArr = [];
+//   highScoreList.innerHTML = "";
+//   // creates variable for value input of initials entered, minus white spaces
+//   var initialText = initialInputEl.value.trim();
+
+//   // adds input to initial array
+//   initialArr.push(initialText);
+//   // creates list item
+//   var li = document.createElement("li");
+//   // setting text of the li to the current initial entered
+//   li.textContent = "Score: " + curScore.textContent + "  Name: " + initialText;
+//   // places list item in high score list
+//   highScoreList.appendChild(li);
+// }
+
+// listen to button to clear high score history 
 clearHistoryBtn.addEventListener("click", function () {
   localStorage.clear();
   highScoreList.innerHTML = "";
 });
 
-// function init() {
-//   renderHighScores();
-// }
+
+function init() {
+  displayHighScores();
+  startScreenEl.classList.remove("hide");
+  endScreenEl.classList.add("hide");
+  currentQuestionIndex = 0;
+}
+
+init();
